e8ebc951aee3e714163b595b30a58b06129808f7
Aymen <aymenfadhel@yahoo.com>
2018-08-28 11:10:00 -0400

    Adding Adaptive EnTK (PHASE 2) to the pipeline

--- EnTKscript/ASIFTv2_EnTk.py
+++ EnTKscript/ASIFTv2_EnTk.py
@@ -1,18 +1,76 @@
-# ASIFTv2 code
-# EntK script 7.3 (PHASE 1)
-
-
 from radical.entk import Pipeline, Stage, Task, AppManager
 import os
 import argparse
-#------------------------------------------------------------------------------
+
+# ------------------------------------------------------------------------------
 # Set default verbosity
 
 if os.environ.get('RADICAL_ENTK_VERBOSE') == None:
     os.environ['RADICAL_ENTK_VERBOSE'] = 'INFO'
 
+os.environ['RADICAL_ENTK_PROFILE'] = 'True'
+
+if not os.environ.get('RADICAL_PILOT_DBURL'):
+    os.environ['RADICAL_PILOT_DBURL'] = "mongodb://********************************/radicallab"
 
 def generate_pipeline(name, stages):
+  
+    #condition function
+    def func_condition():
+       
+        print 'Condition Function Started'    
+        my_path = "/home/usr/ASIFT/PHASE_1_KEYPOINT_GENERATION/fast_imas_IPOL/build/data_matches.csv"
+
+        #if os.path.exists(my_path) and os.path.getsize(my_path) > 0:
+        if os.path.exists(my_path) and os.path.getsize(my_path) > 0:
+            return True
+            
+
+        return False
+
+    #True Condition function
+    def func_on_true():
+        print 'True Function'
+
+        s2 = Stage()
+        t = Task()    
+        t.executable = ['/bin/echo']   
+        t.cpu_reqs['process_type'] = ''
+        t.cpu_reqs['thread_type'] = ''
+        t.arguments = ["This is Adaptive Stage on True"] 
+        t.cores = 20
+
+        # Add the Task to the Stage
+        s2.add_tasks(t)
+
+        # Add post-exec to the Stage
+
+        p.add_stages(s2)
+
+    #False Condition function
+    def func_on_false():
+        print 'False Function'
+
+        s1=Stage()
+
+
+        # Create a Stage object 
+            
+        t1 = Task()    
+        t1.executable = ['/bin/echo']   
+        t1.cpu_reqs['process_type'] = ''
+        t1.cpu_reqs['thread_type'] = ''
+        t1.arguments = ["This is Adaptive Stage on False"] 
+        t1.cores = 20
+
+        # Add the Task to the Stage
+        s1.add_tasks(t1)
+
+        # Add post-exec to the stage
+        # Add Stage to the Pipeline
+        p.add_stages(s1)        
+
+
 
     # Create a Pipeline object
     p = Pipeline()
@@ -25,9 +83,17 @@ def generate_pipeline(name, stages):
     # Create a Task object
     t = Task()
     t.name = 'task1'        # Assign a name to the task (optional)
-    t.executable ='/ASIFT_V2/ASIFT/PHASE_1_KEYPOINT_GENERATION/fast_imas_IPOL/build/main'   # Assign executable to the task
+    t.executable ='/home/usr/SummerRadical/ASIFT/PHASE_1_KEYPOINT_GENERATION/fast_imas_IPOL/build/main'   # Assign executable to the task
+    t.cpu_reqs['process_type'] = ''
+    t.cpu_reqs['thread_type'] = ''
     t.arguments = ['-im1','input_0.png','-im2' ,'input_1.png','-desc',11,'-covering' ,1.4]# Assign arguments
     t.upload_input_data= ['input_0.png', 'input_1.png','covering.png']
+    t.download_output_data = ['data_matches.csv']
+    s.post_exec = {
+                             'condition': func_condition,
+                             'on_true': func_on_true,
+                             'on_false': func_on_false
+                    }
 
     # Add the Task to the Stage
     s.add_tasks(t)
@@ -35,7 +101,9 @@ def generate_pipeline(name, stages):
     # Add Stage to the Pipeline
     p.add_stages(s)
 
-    return p
+
+    return p 
+
 
 if __name__=='__main__':
 
@@ -50,27 +118,32 @@ if __name__=='__main__':
 
     res_dict = {
 
-                  'resource': 'xsede.comet',
+              
+                  'resource': 'xsede.comet_anaconda',
                   'walltime': 10,
                   'cpus'   : args.cores,
                   'project' : '',
-                  'queue'   : 'debug',
+                  'queue'   : 'compute',
                   'schema'  :'gsissh',
 
-               }
+               } 
 
     # Create Application Manager
     appman = AppManager(port=32769,hostname='localhost')
-
+ 
     # Assign resource manager to the Application Manager
     appman.resource_desc = res_dict
 
     pipelines = list()
     for cnt in range(args.pipelines):
         p1 = generate_pipeline(name ='Pipeline%s'%cnt,stages = 1)
+        #dev = dev ^ 1
         pipelines.append(p1)
+        
     # Assign the workflow as a set of Pipelines to the Application Manager
     appman.workflow=set(pipelines)
 
     # Run the Application Manager
     appman.run()
+
+
